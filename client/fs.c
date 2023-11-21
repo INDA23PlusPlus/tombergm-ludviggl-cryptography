aafa97ea66ccebc50e04b87d71bfe826127cc6a4
Ludvig Gunne Lindström <ludvig.gunne.lindstrom@proton.me>
2023-11-21 15:43:26 +0100

    implement file system

--- client/fs.c
+++ client/fs.c
@@ -1,33 +1,49 @@
 
 #include "fs.h"
+#include "cache.h"
+#include "client.h"
+
+#include <string.h>
 
 #define get_block(id) 0
 
+#define verify_ptr(ptr)\
+    ({\
+        void *__ptr = ptr;\
+        if (__ptr == NULL) return -FSERR_IO;\
+        __ptr;\
+    })
+
 int fs_init(void)
 {
+    return 0;
 }
 
-static unsigned block_alloc()
+static unsigned block_alloc(client_t *cl)
 {
-    fs_super_t super_ptr = fs_super_ptr(get_block(0));
-    if (super_ptr->free_count == 0) return 0;
+    fs_super_t *super = verify_ptr(cache_get_blk(cl->sb_cache, 0));
 
-    unsigned char *map_ptr = &super_ptr->blocks;
-    unsigned map_id = 0;
-    unsigned block_offset = 0;
+    unsigned map_count = super->map_count;
+    unsigned map_id = 1;
+    unsigned char *map = verify_ptr(cache_get_blk(cl->sb_cache, map_id));
 
-    for (unsigned i = BLOCK_OFFSET; i < super_ptr->total_count; i++)
+    for (unsigned byte_id = map_count; byte_id < BLOCK_SIZE * map_count; byte_id++)
     {
-        unsigned char byte = super_ptr->blocks[i];
-        if (byte == 0xFF) continue;
+        map_id = byte_id / BLOCK_SIZE;
+        if (byte_id % BLOCK_SIZE == 0)
+        {
+            map = verify_ptr(cache_get_blk(cl->sb_cache, map_id));
+        }
+
+        unsigned char *byte = &map[byte_id % BLOCK_SIZE];
+        if (*byte == 0xFF) continue;
 
         for (unsigned offset = 0; offset < 8; offset++)
         {
-            if (!(byte & 1))
+            if (!((*byte >> offset) & 1))
             {
-                super_ptr->free_count--;
-                super_ptr->blocks[i] |= (1 << offset);
-                return (i << 3) & offset;
+                *byte |= (1 << offset);
+                return byte_id * 8 + offset;
             }
         }
     }
@@ -35,20 +51,15 @@ static unsigned block_alloc()
     return 0;
 }
 
-static void block_free(unsigned id)
+static void block_free(client_t *cl, unsigned id)
 {
-    unsigned byte_id = id >> 3;
-    unsigned offset = id & 7;
-    fs_super_t super_ptr = fs_super_ptr(get_block(0));
-    super_ptr->blocks[byte_id] &= ~(1 << offset);
-    super_ptr->free++; // watch out for double free
 }
 
 static int parse_name(const char **path)
 {
     if (**path == '\0') return 0;
 
-    while (**path == '\\') *path++;
+    while (**path == '\\') (*path)++;
     while (**path != '\\')
     {
         if (**path == '\0')
@@ -60,9 +71,11 @@ static int parse_name(const char **path)
     return 1;
 }
 
-int fs_find_block(unsigned root, const char *path, unsigned *id)
+int fs_find_block(client_t *cl, unsigned root, const char *path, unsigned *id, unsigned *type)
 {
-    *id = root;
+    fs_super_t *super = verify_ptr(cache_get_blk(cl->sb_cache, 0));
+    *id = super->root;
+    fs_dir_t *dir = verify_ptr(cache_get_blk(cl->dir_cache, *id));
 
     for (;;)
     {
@@ -70,33 +83,31 @@ int fs_find_block(unsigned root, const char *path, unsigned *id)
         int contin = parse_name(&path);
         unsigned name_len = path - begin;
 
-        fs_block_t *block_ptr = fs_block_ptr(get_block(id));
-        if (block_ptr->type != DIR)
-        {
-            return -FS_NOT_DIR;
-        }
-
-        fs_dir_t *dir_ptr = fs_dir_ptr(block_ptr);
-
         int matched = 0;
 
-        for (unsigned i = 0; i < dir_ptr->entry_count; i++)
+        for (unsigned i = 0; i < dir->entry_count; i++)
         {
-            fs_dir_entry_t *entry = &dir_ptr->entries[i];
+            fs_dir_entry_t *entry = &dir->entries[i];
 
-            if (!entry->used) continue;
+            if (entry->free) continue;
 
             unsigned entry_name_len = strlen(entry->name);
 
             if (entry_name_len == name_len && memcmp(entry->name, begin, name_len) == 0)
             {
                 *id = entry->id;
+                *type = entry->type;
+                if (*type == FS_FILE)
+                {
+                    return 0;
+                }
+                dir = verify_ptr(cache_get_blk(cl->dir_cache, *id));
                 matched = 1;
                 break;
             }
         }
 
-        if (!matched) return -FS_NOT_FOUND;
+        if (!matched) return -FSERR_NOT_FOUND;
 
         if (!contin)
         {
@@ -107,38 +118,189 @@ int fs_find_block(unsigned root, const char *path, unsigned *id)
     return 0;
 }
 
-int fs_create_dir(unsigned dir, const char *name)
+int fs_create_dir(client_t *cl, unsigned dir, const char *name)
 {
-    fs_super_t *super_ptr = fs_super_ptr(get_block(0));
+    fs_super_t *super_ptr = verify_ptr(cache_get_blk(cl->sb_cache, 0));
 
-    unsigned name_len = strlen(name);
+    unsigned name_len = strlen(name) + 1; // include \0
 
-    if (> NAME_MAX_LEN - 1) return -FS_LONG_NAME;
-    if (super_ptr->free_count == 0) return -FS_OOM;
+    if (name_len > NAME_MAX_LEN) return -FSERR_LONG_NAME;
+    if (super_ptr->free_count == 0) return -FSERR_OOM;
 
-    fs_block_t *block_ptr = fs_block_ptr(get_block(dir));
-    if (block_ptr->type != DIR) return -FS_NOT_DIR;
-
-    fs_dir_t *dir_ptr = fs_dir_ptr(block_ptr);
-    if (dir_ptr->entry_count == DIR_MAX_ENTRIES) return -FS_FULL_DIR;
+    fs_dir_t *dir_ptr = verify_ptr(cache_get_blk(cl->dir_cache, dir));
+    if (dir_ptr->entry_count == DIR_MAX_ENTRIES) return -FSERR_FULL_DIR;
 
     for (unsigned i = 0; i <= dir_ptr->entry_count; i++)
     {
-        fs_dir_entry_t entry = &dir_ptr->entries[i];
-        if (!entry->used)
+        fs_dir_entry_t *entry = &dir_ptr->entries[i];
+        if (entry->free)
         {
-            unsigned id = block_alloc();
-            if (id == 0) return -FS_OOM; // redundant?
-            entry->id = id;
-            memcpy(entry->name, name,
+            unsigned id = block_alloc(cl);
+            if (id == 0) return -FSERR_OOM;
+
+            fs_dir_t *this_dir = verify_ptr(cache_get_blk(cl->dir_cache, id));
+
+            memset(this_dir, 0, sizeof*(this_dir));
+            this_dir->parent     = dir;
+            this_dir->entry_id   = i;
+            this_dir->free_count = DIR_MAX_ENTRIES;
+
+            entry->id   = id;
+            entry->type = FS_DIR;
+
+            memcpy(entry->name, name, name_len);
+
+            dir_ptr->free_count--;
+            return 0;
         }
     }
+
+    return 0;
 }
 
-int fs_create_file(unsigned dir, const char * name)
+int fs_create_file(client_t *cl, unsigned dir, const char *name)
 {
+    fs_super_t *super_ptr = verify_ptr(cache_get_blk(cl->sb_cache, 0));
+
+    unsigned name_len = strlen(name) + 1; // include \0
+
+    if (name_len > NAME_MAX_LEN) return -FSERR_LONG_NAME;
+    if (super_ptr->free_count == 0) return -FSERR_OOM;
+
+    fs_dir_t *dir_ptr = verify_ptr(cache_get_blk(cl->dir_cache, dir));
+    if (dir_ptr->entry_count == DIR_MAX_ENTRIES) return -FSERR_FULL_DIR;
+
+    for (unsigned i = 0; i <= dir_ptr->entry_count; i++)
+    {
+        fs_dir_entry_t *entry = &dir_ptr->entries[i];
+        if (entry->free)
+        {
+            unsigned id = block_alloc(cl);
+            if (id == 0) return -FSERR_OOM;
+
+            fs_file_t *file = verify_ptr(cache_get_blk(cl->dir_cache, id));
+
+            file->parent   = dir;
+            file->entry_id = i;
+
+            entry->id = id;
+            entry->type = FS_FILE;
+            memcpy(entry->name, name, name_len);
+
+            dir_ptr->free_count--;
+
+            return 0;
+        }
+    }
+
+    return 0;
 }
 
-int fs_delete_block(unsigned dir)
+int fs_delete_file(client_t *cl, unsigned id)
 {
+    fs_file_t *file_ptr = verify_ptr(cache_get_blk(cl->dir_cache, id));
+    unsigned block_count = file_ptr->size / BLOCK_SIZE;
+
+    for (unsigned i = 0; i < block_count; i++)
+    {
+        block_free(cl, file_ptr->blocks[id]);
+    }
+
+    fs_dir_t *parent = verify_ptr(cache_get_blk(cl->dir_cache, file_ptr->parent));
+    parent->entries[file_ptr->entry_id].free = 1;
+    parent->free_count++;
+    block_free(cl, id);
+
+    return 0;
+}
+
+int fs_delete_dir(client_t *cl, unsigned id)
+{
+    fs_dir_t *dir = verify_ptr(cache_get_blk(cl->dir_cache, id));
+    for (int i = 0; i < DIR_MAX_ENTRIES; i++)
+    {
+        fs_dir_entry_t *entry = &dir->entries[i];
+        if (!entry->free)
+        {
+            switch (entry->type)
+            {
+                case FS_FILE:
+                    fs_delete_file(cl, entry->id);
+                    break;
+                case FS_DIR:
+                    fs_delete_dir(cl, entry->id);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    block_free(cl, id);
+
+    return 0;
+}
+
+int fs_write_file(client_t *cl, unsigned file, const char *buf, size_t size, size_t offset)
+{
+    char *block;
+
+    fs_file_t *file_ptr = verify_ptr(cache_get_blk(cl->dir_cache, file));
+
+    unsigned fsize       = file_ptr->size;
+    unsigned end_offset  = offset + size;
+    unsigned new_size    = end_offset > fsize ? end_offset : fsize;
+    unsigned block_count = fsize / BLOCK_SIZE;
+
+    block = verify_ptr(cache_get_blk(cl->reg_cache, file_ptr->blocks[offset / BLOCK_SIZE]));
+
+    for (size_t fpos = offset, bpos = 0; fpos < size + offset; fpos++, bpos++)
+    {
+        unsigned block_ms = fpos / BLOCK_SIZE;
+        unsigned block_ls = fpos % BLOCK_SIZE;
+
+        if (block_ms > block_count)
+        {
+            unsigned new_block_id = block_alloc(cl);
+            if (new_block_id == 0) return -FSERR_OOM;
+
+            block_count++;
+            file_ptr->blocks[block_ms] = new_block_id;
+        }
+
+        if (block_ls == 0)
+        {
+            block = verify_ptr(cache_get_blk(cl->reg_cache, file_ptr->blocks[block_ms]));
+        }
+
+        block[block_ls] = buf[bpos];
+    }
+
+    file_ptr->size = new_size;
+
+    return 0;
+}
+
+int fs_read_file(client_t *cl, unsigned file, char *buf, size_t size, size_t offset)
+{
+    char *block;
+    fs_file_t *file_ptr = verify_ptr(cache_get_blk(cl->dir_cache, file));
+
+    if (offset + size > file_ptr->size) return -FSERR_OVERFLOW;
+
+    block = verify_ptr(cache_get_blk(cl->reg_cache, file_ptr->blocks[offset / BLOCK_SIZE]));
+
+    for (size_t fpos = offset, bpos = 0; fpos < size + offset; fpos++, bpos++)
+    {
+        unsigned block_ms = fpos / BLOCK_SIZE;
+        unsigned block_ls = fpos % BLOCK_SIZE;
+
+        if (block_ls == 0)
+        {
+            block = verify_ptr(cache_get_blk(cl->reg_cache, file_ptr->blocks[block_ms]));
+        }
+
+        buf[bpos] = block[block_ls];
+    }
+
+    return 0;
 }
